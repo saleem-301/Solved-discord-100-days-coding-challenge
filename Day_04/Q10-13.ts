089fb561aecde3b4367f95bd560862b6f30feba4
saleem-301 <adnanyousuf301@gmail.com>
2024-03-18 14:58:42 +0500

    Q10-12

---
+++ Day_04/Q10-13.ts
@@ -0,0 +1,21 @@
+//Question 10:
+//MUHAMMAD SALEEM Dated : 01-Mar-2024
+//Print a Sentence
+console.log('Pakistan Zindabad');
+
+//Question 11:
+console.log('\n');
+let name3 = ['Faisal','Hammad','Ahmed','Mehmood']
+console.log(name3[0]);
+console.log(name3[1]);
+console.log(name3[2]);
+console.log(name3[3]);
+
+
+//Question 12:
+console.log('\n');
+let name4 = ['Faisal','Hammad','Ahmed','Mehmood']
+console.log(`Mr ${name4[0]} you are invited to learn typescript`);
+console.log(`Mr ${name4[1]} you are invited to learn typescript`);
+console.log(`Mr ${name4[2]} you are invited to learn typescript`);
+console.log(`Mr ${name4[3]} you are invited to learn typescript`);
