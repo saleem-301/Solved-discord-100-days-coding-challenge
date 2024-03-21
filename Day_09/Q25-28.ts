a6d8ddaeb837dde5a8593747fbc00f9b3252ea23
saleem-301 <157378495+saleem-301@users.noreply.github.com>
2024-03-21 14:28:40 +0500

    Q25-28

---
+++ Day_09/Q25-28.ts
@@ -0,0 +1,66 @@
+
+//Question 25:
+let alien_color = 'green';
+if (alien_color == 'green'){
+    console.log('Player Just Earned 5 Points')
+}
+else{
+    console.log('Try again')
+}
+//Question 26:  
+alien_color = 'yellow';
+if (alien_color == 'green'){
+    console.log('Player Just Earned 5 Points')
+}
+else{}
+
+
+
+//Question 27:
+// let alien_color = 'green'
+    if (alien_color == 'green'){
+    console.log('Player Just Earned 5 Points')}
+else{
+    console.log('Player Just Earned 10 Points')};
+
+alien_color = 'yellow'
+    if (alien_color == 'green'){
+    console.log('Player Just Earned 5 Points')}
+
+    else if (alien_color == 'yellow'){
+    console.log('Player Just Earned 10 Points')}
+
+    else{
+    console.log('Player Just Earned 15 Points')};
+
+alien_color = 'red';
+    if (alien_color == 'green'){
+    console.log('Player Just Earned 5 Points')}
+    
+    else if (alien_color == 'yellow'){
+    console.log('Player Just Earned 10 Points')}
+
+    else{
+    console.log('Player Just Earned 15 Points')};
+
+
+//Question 28:
+console.log('\n');
+const  Age = 65;
+if (Age < 2) {
+    console.log("The Peron is Baby")
+}
+else if (Age <4) {
+    console.log("The Peron is Toddler")
+}
+else if (Age < 13) {
+    console.log("The Peron is Kid")
+}
+else if (Age < 20) {
+    console.log("The Peron is TeenAger")
+}
+else if (Age < 65) {
+    console.log("The Peron is Adult")
+    }
+else {
+        console.log("The Peron is Elder")};
