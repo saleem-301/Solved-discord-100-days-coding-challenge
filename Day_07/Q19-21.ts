da1b9b28ca5df6cfc76e09e3084c4248c0c3e9aa
saleem-301 <157378495+saleem-301@users.noreply.github.com>
2024-03-21 14:23:02 +0500

    Q19-21

---
+++ Day_07/Q19-21.ts
@@ -0,0 +1,18 @@
+//Question 19:
+var guests = ['HAMAD','AHMED','SUBHAN'];
+console.log('\nList of Invited People \n\t',guests,'\n');
+
+console.log('No of Guest Invited = ', guests.length);
+
+//Question 20:
+
+let list = ['k2','MountEverst','Himalya'];
+console.log(list);
+
+//Question 21:
+let Mounts = ['k2','MountEverst','Himalya'];
+let rivers = ['jehlum','chena','sindh', 'satlej']
+
+let objs = {1:Mounts,2:rivers};
+
+console.log(objs[1]);
